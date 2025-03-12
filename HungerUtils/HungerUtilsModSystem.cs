ac75e1e5d4d5c336490c48424780637417ce3017
Zach2039 <16394465+zach2039@users.noreply.github.com>
2025-03-11 19:44:49 -0500

    Initial commit

---
+++ HungerUtils/HungerUtilsModSystem.cs
@@ -0,0 +1,17 @@
+﻿using HungerUtils.ModCommands;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace HungerUtils
+{
+    public class HungerUtilsModSystem : ModSystem
+    {
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            HungerCommands.Register(api);
+            Mod.Logger.Notification("[HungerUtils] Loaded server-side");
+        }
+    }
+}
---
+++ HungerUtils/ModCommands/HungerCommands.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+
+namespace HungerUtils.ModCommands
+{
+    public static class HungerCommands
+    {
+        private static string[] hungerUtilValidAttributes =
+        {
+            "Saturation",
+            "SaturationLossDelayFruit",
+            "SaturationLossDelayVegetable",
+            "SaturationLossDelayProtein",
+            "SaturationLossDelayGrain",
+            "SaturationLossDelayDairy",
+            "MaxSaturation",
+            "FruitLevel",
+            "VegetableLevel",
+            "ProteinLevel",
+            "GrainLevel",
+            "DairyLevel"
+        };
+
+        public static void Register(ICoreServerAPI api)
+        {
+            api.ChatCommands
+                .Create("hungerutils")
+                .WithDescription("Configuration utils for player hunger.")
+                .RequiresPrivilege("controlserver")
+                .WithArgs(
+                    api.ChatCommands.Parsers.Word("playerName"),
+                    api.ChatCommands.Parsers.WordRange("hungerUtilAttribute", hungerUtilValidAttributes),
+                    api.ChatCommands.Parsers.OptionalFloat("setValue", float.NaN)
+                    )
+                .HandleWith((args) => OnHungerUtilsCommand(api, args));
+        }
+
+        private static IServerPlayer GetPlayerByName(ICoreServerAPI api, string playerName)
+        {
+            foreach (IServerPlayer player in api.World.AllOnlinePlayers)
+            {
+                if (player.PlayerName.Equals(playerName, StringComparison.OrdinalIgnoreCase))
+                {
+                    return player;
+                }
+            }
+            return null;
+        }
+
+        private static TextCommandResult OnHungerUtilsCommand(ICoreServerAPI api, TextCommandCallingArgs args)
+        {
+            string hungerUtilAttribute = args[0] as string;
+            string playerName = args[1] as string;
+            float setValue = (float)args[2];
+
+            IServerPlayer targetPlayer;
+
+            if (string.IsNullOrEmpty(playerName))
+            {
+                targetPlayer = args.Caller.Player as IServerPlayer;
+            }
+            else
+            {
+                targetPlayer = GetPlayerByName(api, playerName);
+                if (targetPlayer == null)
+                {
+                    return TextCommandResult.Error($"Player '{playerName}' not found.");
+                }
+            }
+
+            var behaviorHunger = targetPlayer.Entity.GetBehavior<EntityBehaviorHunger>();
+            if (behaviorHunger == null) return TextCommandResult.Error("Hunger behavior not found.");
+
+
+            if (float.IsNaN(setValue))
+            {
+                // Get value instead of set
+                PropertyInfo propertyInfo = typeof(EntityBehaviorHunger).GetProperty(hungerUtilAttribute);
+
+                if (propertyInfo != null)
+                {
+                    float retValue = (float)propertyInfo.GetValue(behaviorHunger);
+                    return TextCommandResult.Success($"Value for '{hungerUtilAttribute}' is {retValue} for player '{targetPlayer.PlayerName}'.");
+                }
+                else
+                {
+                    return TextCommandResult.Error($"Could not locate EntityBehaviorHunger property '{hungerUtilAttribute}' for Player '{playerName}'.");
+                }
+            }
+            else
+            {
+                // Set value
+                PropertyInfo propertyInfo = typeof(EntityBehaviorHunger).GetProperty(hungerUtilAttribute);
+
+                if (propertyInfo != null)
+                {
+                    propertyInfo.SetValue(behaviorHunger, setValue);
+                    return TextCommandResult.Success($"'{hungerUtilAttribute}' set to {setValue} for player '{targetPlayer.PlayerName}'.");
+                }
+                else
+                {
+                    return TextCommandResult.Error($"Could not locate EntityBehaviorHunger property '{hungerUtilAttribute}' for Player '{playerName}'.");
+                }
+            }
+        }
+    }
+}
